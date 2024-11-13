535999d30aa7d44dfe9ed1b58a6180ede8f0fc8c
Milan-C-I <milancheriyamanep@gmail.com>
2024-11-13 14:31:00 +0530

    auth fixed

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,26 +1,24 @@
 import { logIn } from "@/backend";
-import NextAuth, { AuthOptions } from "next-auth"
-import Credentials from "next-auth/providers/credentials";
-// import Google from "next-auth/providers/google";
+import NextAuth, { AuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 
-export const authOptions:AuthOptions = {
-  // Configure one or more authentication providers
+const authOptions: AuthOptions = {
   providers: [
-    Credentials({
-        name: "Credentials",
-        credentials: {},
-        async authorize(credentials,req) {
-            let {username, password} = credentials as any;
-            let user = await logIn({username, password});
-            if (!user) {
-                return null;
-            }
-            return {id: user._id, ...user};
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {},
+      async authorize(credentials, req) {
+        const { username, password } = credentials as any;
+        const user = await logIn({ username, password });
+        if (!user) {
+          return null;
+        }
+        return { id: user._id, ...user };
       },
     }),
   ],
-}
+};
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
